2ad163f6443fd76a95cea111ce5c973477888730
Liviu Miu <liviumiu@gmail.com>
2018-12-27 22:54:01 +0200

    new unittest project: dummy_project

--- runner/web_gui/models.py
+++ runner/web_gui/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
+
 class Project(models.Model):
     PROJECTS = (
         ('Selenium Project', 'Selenium Project'),
