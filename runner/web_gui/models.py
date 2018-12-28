1d1b394f1d86c7f92a881d1ef925cf5a2ab5b204
MIU Catalin <catalin.miu@brd.ro>
2018-12-28 16:21:38 +0200

    added project page and models for Run and runed_test

--- runner/web_gui/models.py
+++ runner/web_gui/models.py
@@ -17,3 +17,19 @@ class Test(models.Model):
     title = models.CharField(max_length=60)
     description = models.TextField
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    running_now = models.BooleanField(default=False)
+
+
+class Run(models.Model):
+    runned_at = models.DateTimeField(default=datetime.now, blank=True)
+
+
+class Runed_test(models.Model):
+    run_id = models.ForeignKey(Run, on_delete=models.CASCADE)
+    test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
+    logs = models.TextField
+    result = models.CharField(max_length=60)
+
+
+
--- runner/web_gui/views.py
+++ runner/web_gui/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Project
 
 
@@ -7,3 +8,9 @@ def index(request):
 
     context = {'project_list': project_list}
     return render(request, 'web_gui/index.html', context)
+
+
+def project(request, project_id):
+    project =get_object_or_404(Project, id=project_id)
+    context = {'project': project}
+    return render(request, 'web_gui/project.html', context)
